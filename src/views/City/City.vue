2510c8ce2b2475d9d2e4d8dae711d9b63bb9eddd
xiaoqingb <975947596@qq.com>
2019-10-19 15:17:02 +0800

    添加城市搜索框

--- src/views/City/City.vue
+++ src/views/City/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header ></city-header>
+    <city-search ></city-search>
   </div>
   
 </template>
 
 <script>
 import cityHeader from './compoments/Header.vue'
+import CitySearch from './compoments/Search.vue'
 export default {
     name:"City",
     components:{
       cityHeader,
+      CitySearch
     }
 }
 </script>
