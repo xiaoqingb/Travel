69d72954d10b79ff1612b7ac9729192f9c6a0938
xiaoqingb <975947596@qq.com>
2019-10-20 00:44:39 +0800

    完成城市搜索框的逻辑功能

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ stylus
 webpack
 awesome
 axios
-mock...4
+mock
 swiper
 better-scroll
 函数节流
+
+
 ####2019-10-18 23：30  ---随笔
 ###接触了几个新东西，比如：
 
@@ -20,13 +22,16 @@ better-scroll
  模拟数据传输的一个工具，在vue-cli@3之前，是在static目录里建立的，但是我现在这个4.0版本目录已经没有config，build，static这些目录了，
  硬着头皮上网查，真的给我查到了个mock这个东西，使用方法如下：
  
- step 1 :  先在main.js里建立一个mock.js
+ step 1 :  先在src目录里建立一个mock文件夹
  
- step 2 :  在文件里输入以下代码
+ step 2 :  在文件里创建index.js,然后输入以下代码
  
     import Mock from 'mockjs';
-    Mock.mock('/index', { //输出数据
+    Mock.mock('/city', { //输出数据
         city:'北京', 
+    });
+    Mock.mock('/login', { //输出数据
+        account:'xxx', 
     });`
    
  step 3 : 在接收axios的组件里写入以下代码
@@ -46,3 +51,5 @@ better-scroll
     }`
  
  ### vue-cli更新后webpack的目录和配置文件都没有啦？？？？
+ ### 4.better-scroll
+ 当下移动端流行的滚动条样式，这里就不做详述了，有官方文档可以看
--- src/views/City/compoments/Search.vue
+++ src/views/City/compoments/Search.vue
@@ -1,19 +1,72 @@
 <template>
-  <div class="search">
-        <input class="search-input" name="" type='text' placeholder="输入城市名或拼音"/>
-  </div>
+    <div>
+        <div class="search">
+                <input v-model="keyword" class="search-input" name="" type='text' placeholder="输入城市名或拼音"/>
+        </div>
+        <div class="search-content" ref="SearchWrapper"
+            v-show="keyword">
+            <ul>
+                <li class="item" v-for="item of list" :key="item.id">{{item.name}}</li>
+                <li class="item" v-show="hasData">
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 
 export default {
     name:'CitySearch',
+    data(){
+        return{
+            timer:null,
+            keyword:'',
+            list:[],
+        }
+    },
+    props:{
+        cities:'',
+    },
+    computed:{
+        hasData(){
+            return !this.list.length
+        }
+    },
+    watch:{
+        keyword(){
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            if (!this.keyword) {
+                this.list=[];
+                return;
+            }
+            this.timer=setTimeout(() => {
+                const result=[];
+                for(let i in this.cities){
+                    this.cities[i].forEach((val)=>{
+                        if (val.spell.indexOf(this.keyword) > -1||val.name.indexOf(this.keyword) > -1) {
+                            result.push(val);
+                        }
+                    })
+                }
+                this.list=result;
+            }, 100);
+        }
+    },
+     mounted(){
+      this.scroll=new BScroll(this.$refs.SearchWrapper)
+  },
 }
 </script>
 
 <style lang='less' scoped>
 @import url('~@/assets/styles/varibles.less');
 .search{
+    z-index:2;
     background: @bgColor;
     height: 33px;
     padding:0 5px  4px 5px;
@@ -23,9 +76,30 @@ export default {
         height: 100%;
         border-radius: 5px;
         text-align: center;
-        color: rgba(119, 119, 119, 0.726);
-
+        color: rgba(119, 119, 119);
     }
 }
+.search-content{
+    z-index:1;
+    overflow: hidden;
+    position: absolute;
+    top: 80px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #fff;
+    .item{
+    height: 33px;
+      line-height: 33px;
+      padding-left: 8px;
+      font-size: 15px;
+      color: rgb(88, 88, 88);
+      border-bottom: rgba(204, 204, 204, 0.527) 0.5px solid;
+    }
+}
+
+
+    
+
 
 </style>
