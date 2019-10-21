0ef5c7269aaded3fd88808e14dd0deca12583703
xiaoqingb <975947596@qq.com>
2019-10-21 22:06:45 +0800

    完成景点详情图片的开发

---
+++ src/common/gallary/gallary.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper  :options='swiperOptions'>
+        <swiper-slide v-for="(item,index) of bannerImgs" :key="index">
+            <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonGallary",
+  props:{
+      bannerImgs:''
+  },
+  data(){
+      return {
+          swiperOptions:{
+              pagination:'.swiper-pagination',
+              paginationType:'fraction',
+              observeParents:true,
+              observer:true,
+          },
+          
+      }
+  },
+  methods:{
+      handleGallaryClick(){
+          this.$emit('close')
+      }
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.container {
+    /deep/.swiper-container{
+        overflow: inherit;
+    }
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 1;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: #000;
+  .wrapper {
+
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+    .gallary-img{
+        // width: 100%;
+        height: 100%;
+    }
+    .swiper-pagination{
+        z-index: 2;
+        color: #ccc;
+        position: absolute;
+        bottom: -40%;
+    } 
+}
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home/Home.vue'
-import City from '../views/City/City.vue'
+import Home from '@/views/Home/Home.vue'
+import City from '@/views/City/City.vue'
+import Detail from '@/views/Detail/Detail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
   },
   {
     path: '/city',
     name: 'City',
     component: City
+  },
+  {
+    // 动态路由
+    path: '/detail',
+    name: 'Detail',
+    component: Detail
   },
 
 ]
